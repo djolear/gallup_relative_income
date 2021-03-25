0bb5704e76b35385446e24af141ae9bf28c737f6
Daniel O'Leary <djolear@stanford.edu>
2021-03-25 16:13:25 -0700

    ic

---
+++ preprocessing/demographically_derived_median_income_function.R
@@ -0,0 +1,30 @@
+calculate_median_income <- function(df){
+  med_inc_gallup <-
+    df %>% 
+    mutate(
+      age_dec = round(age, digits = -1)
+    ) %>% 
+    group_by(
+      age_dec,
+      sex,
+      education
+    ) %>% 
+    summarise(
+      med_inc_demo_age_gallup = matrixStats::weightedMedian(income, w = COMB_WEIGHT, na.rm = TRUE)
+    ) %>% 
+    ungroup() %>% 
+    filter_all(all_vars(!is.na(.)))
+  
+  df <-
+    df %>% 
+    mutate(
+      age_dec = round(age, digits = -1)
+    ) %>% 
+    left_join(
+      med_inc_gallup,
+      by = c("education", "sex", "age_dec")
+    )
+  
+  return(df)
+}
+
