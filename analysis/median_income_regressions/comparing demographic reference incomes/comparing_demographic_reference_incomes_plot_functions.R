f78639ef0a0e624a93dbc735d2fcb3582464a06f
Daniel O'Leary <djolear@stanford.edu>
2021-05-31 11:28:02 -0700

    ic

---
+++ analysis/median_income_regressions/comparing demographic reference incomes/comparing_demographic_reference_incomes_plot_functions.R
@@ -0,0 +1,104 @@
+coef_plot <- function(outcome, data){
+  plot <-
+    data %>% 
+    dplyr::select(
+      mi_var, 
+      outcome = outcome, 
+      outcome_key
+    ) %>% 
+    mutate(
+      outcome = abs(outcome),
+      mi_var =
+        case_when(
+          mi_var == "median_income_demo_esa_scale" ~ "age, sex, educ",
+          mi_var == "median_income_demo_sar_scale" ~ "age, sex, race",
+          mi_var == "median_income_demo_esar_scale" ~ "age, sex, educ, race",
+          mi_var == "median_income_demo_sarcr_scale" ~ "age, sex, race, region",
+          mi_var == "median_income_demo_esacr_scale" ~ "age, sex, educ, race, region",
+          mi_var == "median_income_demo_sa_scale" ~ "age, sex",
+          mi_var == "median_income_demo_sacr_scale" ~ "age, sex, region",
+          mi_var == "median_income_demo_esaiq_scale" ~ "age, sex, educ, iq",
+          mi_var == "median_income_demo_sariq_scale" ~ "age, sex, race, iq",
+          mi_var == "median_income_demo_saiq_scale" ~ "age, sex, iq"
+        ),
+      outcome_key =
+        case_when(
+          outcome_key == "bmi_scale" ~ "BMI",
+          outcome_key == "fruits_veggies_scale" ~ "fruits/veggies",
+          outcome_key == "ladder_now_scale" ~ "Cantril ladder",
+          outcome_key == "sr_health_scale" ~ "sr health",
+          outcome_key == "PURPOSE_scale" ~ "purpose",
+          outcome_key == "FINANCIAL_scale" ~ "financial",
+          outcome_key == "SOCIAL_scale" ~ "social",
+          outcome_key == "COMMUNITY_scale" ~ "community",
+          outcome_key == "eat_healthy" ~ "healthy eating",
+          outcome_key == "smoke" ~ "smoking",
+          outcome_key == "diabetes" ~ "diabetes",
+          outcome_key == "hbp" ~ "hbp",
+        )
+    ) %>% 
+    ggplot(aes(fct_reorder(mi_var, outcome), outcome)) + 
+    geom_col() + 
+    labs(
+      x = "median income reference",
+      y = "absolute value of coef."
+    ) +
+    coord_flip() + 
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))+ 
+    facet_grid(. ~ outcome_key, scales = "free")
+  
+  return(plot)
+}
+
+
+coef_diff_plot <- function(outcome, data){
+  plot <-
+    data %>% 
+    dplyr::select(
+      mi_var, 
+      outcome = outcome, 
+      outcome_key
+    ) %>% 
+    mutate(
+      outcome = abs(outcome),
+      mi_var =
+        case_when(
+          mi_var == "median_income_demo_esa_scale" ~ "age, sex, educ",
+          mi_var == "median_income_demo_sar_scale" ~ "age, sex, race",
+          mi_var == "median_income_demo_esar_scale" ~ "age, sex, educ, race",
+          mi_var == "median_income_demo_sarcr_scale" ~ "age, sex, race, region",
+          mi_var == "median_income_demo_esacr_scale" ~ "age, sex, educ, race, region",
+          mi_var == "median_income_demo_sa_scale" ~ "age, sex",
+          mi_var == "median_income_demo_sacr_scale" ~ "age, sex, region",
+          mi_var == "median_income_demo_esaiq_scale" ~ "age, sex, educ, iq",
+          mi_var == "median_income_demo_sariq_scale" ~ "age, sex, race, iq",
+          mi_var == "median_income_demo_saiq_scale" ~ "age, sex, iq"
+        ),
+      outcome_key =
+        case_when(
+          outcome_key == "bmi_scale" ~ "BMI",
+          outcome_key == "fruits_veggies_scale" ~ "fruits/veggies",
+          outcome_key == "ladder_now_scale" ~ "Cantril ladder",
+          outcome_key == "sr_health_scale" ~ "sr health",
+          outcome_key == "PURPOSE_scale" ~ "purpose",
+          outcome_key == "FINANCIAL_scale" ~ "financial",
+          outcome_key == "SOCIAL_scale" ~ "social",
+          outcome_key == "COMMUNITY_scale" ~ "community",
+          outcome_key == "eat_healthy" ~ "healthy eating",
+          outcome_key == "smoke" ~ "smoking",
+          outcome_key == "diabetes" ~ "diabetes",
+          outcome_key == "hbp" ~ "hbp",
+        )
+    ) %>% 
+    ggplot(aes(fct_reorder(mi_var, outcome), outcome)) + 
+    geom_col() + 
+    labs(
+      x = "median income reference",
+      y = "difference in value of coef."
+    ) +
+    coord_flip() + 
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))+ 
+    facet_grid(. ~ outcome_key, scales = "free")
+  
+  return(plot)
+}
---
+++ analysis/median_income_regressions/comparing demographic reference incomes/many_models_demographic_reference.R
@@ -0,0 +1,651 @@
+many_models_demographic_reference_w_covariates <- function(outcomes, df){
+  df <-
+    df %>%
+    filter_at(
+      vars(
+        ladder_now_scale,
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        education_scale,
+        raw_income_scale,
+        median_home_value_county_scale,
+        median_monthly_housing_cost_county_scale,
+        physicians_scale,
+        total_pop_county_scale,
+        land_area_2010_scale,
+        race,
+        sex,
+        age_scale,
+        married,
+        employment_all,
+        year
+      ),
+      all_vars(!is.na(.))
+    )
+  
+  df <- 
+    df %>%
+    sample_n(500000) %>% 
+    gather(outcome_key, value, outcomes)
+  
+  df <-
+    df %>% 
+    gather(
+      mi_var, 
+      mi_value, 
+      c(
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        # median_income_demo_cr_scale,
+        # median_income_demo_st_scale,
+        # median_income_demo_iq_scale
+      )
+    )
+  
+  input_fun_lm <- function(data, outcome, predictor, covariates){
+    lm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data)
+  }
+  
+  input_fun_glm <- function(data, outcome, predictor, covariates){
+    glm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data, family = binomial())
+  }
+  
+  models <- 
+    df %>% 
+    group_by(outcome_key, mi_var) %>% 
+    nest %>%
+    mutate(
+      model1 = 
+        map(
+          data, 
+          ~input_fun_lm(
+            data = .,
+            outcome = "scale(value)",
+            predictor = "scale(mi_value)",
+            covariates = 
+              c(
+                "scale(income)", 
+                "scale(education)",
+                "scale(age)",
+                "employment_all",
+                "race",
+                "sex",
+                "married",
+                "scale(total_pop_county)",
+                "scale(median_monthly_housing_cost_county)",
+                "scale(land_area_2010)",
+                "physicians_scale",
+                "year"
+              )
+          ) %>%
+            tidy %>%
+            dplyr::select(term, estimate) %>%
+            spread(term, estimate)
+        )
+    ) %>%  
+    dplyr::select(-data) %>% 
+    unnest(cols = c(model1))
+  
+  rm(df)
+  
+  return(models)
+}
+
+
+many_models_demographic_reference_wo_covariates <- function(outcomes, df){
+  df <-
+    df %>%
+    filter_at(
+      vars(
+        ladder_now_scale,
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        education_scale,
+        raw_income_scale,
+        median_home_value_county_scale,
+        median_monthly_housing_cost_county_scale,
+        physicians_scale,
+        total_pop_county_scale,
+        land_area_2010_scale,
+        race,
+        sex,
+        age_scale,
+        married,
+        employment_all,
+        year
+      ),
+      all_vars(!is.na(.))
+    )
+  
+  df <- 
+    df %>%
+    sample_n(500000) %>% 
+    gather(outcome_key, value, outcomes)
+  
+  df <-
+    df %>% 
+    gather(
+      mi_var, 
+      mi_value, 
+      c(
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        # median_income_demo_cr_scale,
+        # median_income_demo_st_scale,
+        # median_income_demo_iq_scale
+      )
+    )
+  
+  input_fun_lm <- function(data, outcome, predictor, covariates){
+    lm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data)
+  }
+  
+  input_fun_glm <- function(data, outcome, predictor, covariates){
+    glm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data, family = binomial())
+  }
+  
+  models <- 
+    df %>% 
+    group_by(outcome_key, mi_var) %>% 
+    nest %>%
+    mutate(
+      model1 = 
+        map(
+          data, 
+          ~input_fun_lm(
+            data = .,
+            outcome = "scale(value)",
+            predictor = "scale(mi_value)",
+            covariates = 
+              c(
+                "scale(income)",
+                "year"
+              )
+          ) %>%
+            tidy %>%
+            dplyr::select(term, estimate) %>%
+            spread(term, estimate)
+        )
+    ) %>%  
+    dplyr::select(-data) %>% 
+    unnest(cols = c(model1))
+  
+  rm(df)
+  
+  return(models)
+}
+
+
+many_models_demographic_reference_mediators_w_covariates <- function(outcomes, df){
+  df <-
+    df %>%
+    filter_at(
+      vars(
+        ladder_now_scale,
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        PURPOSE_scale,
+        education_scale,
+        raw_income_scale,
+        median_home_value_county_scale,
+        median_monthly_housing_cost_county_scale,
+        physicians_scale,
+        total_pop_county_scale,
+        land_area_2010_scale,
+        race,
+        sex,
+        age_scale,
+        married,
+        employment_all,
+        year
+      ),
+      all_vars(!is.na(.))
+    )
+  
+  df <- 
+    df %>%
+    sample_n(500000) %>% 
+    gather(outcome_key, value, outcomes)
+  
+  df <-
+    df %>% 
+    gather(
+      mi_var, 
+      mi_value, 
+      c(
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        # median_income_demo_cr_scale,
+        # median_income_demo_st_scale,
+        # median_income_demo_iq_scale
+      )
+    )
+  
+  input_fun_lm <- function(data, outcome, predictor, covariates){
+    lm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data)
+  }
+  
+  input_fun_glm <- function(data, outcome, predictor, covariates){
+    glm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data, family = binomial())
+  }
+  
+  models <- 
+    df %>% 
+    group_by(outcome_key, mi_var) %>% 
+    nest %>%
+    mutate(
+      model1 = 
+        map(
+          data, 
+          ~input_fun_lm(
+            data = .,
+            outcome = "scale(value)",
+            predictor = "scale(mi_value)",
+            covariates = 
+              c(
+                "scale(income)", 
+                "scale(education)",
+                "scale(age)",
+                "employment_all",
+                "race",
+                "sex",
+                "married",
+                "scale(total_pop_county)",
+                "scale(median_monthly_housing_cost_county)",
+                "scale(land_area_2010)",
+                "physicians_scale",
+                "year"
+              )
+          ) %>%
+            tidy %>%
+            dplyr::select(term, estimate) %>%
+            spread(term, estimate)
+        )
+    ) %>%  
+    dplyr::select(-data) %>% 
+    unnest(cols = c(model1))
+  
+  rm(df)
+  
+  return(models)
+}
+
+
+many_models_demographic_reference_mediators_wo_covariates <- function(outcomes, df){
+  df <-
+    df %>%
+    filter_at(
+      vars(
+        ladder_now_scale,
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        PURPOSE_scale,
+        education_scale,
+        raw_income_scale,
+        median_home_value_county_scale,
+        median_monthly_housing_cost_county_scale,
+        physicians_scale,
+        total_pop_county_scale,
+        land_area_2010_scale,
+        race,
+        sex,
+        age_scale,
+        married,
+        employment_all,
+        year
+      ),
+      all_vars(!is.na(.))
+    )
+  
+  df <- 
+    df %>%
+    sample_n(500000) %>% 
+    gather(outcome_key, value, outcomes)
+  
+  df <-
+    df %>% 
+    gather(
+      mi_var, 
+      mi_value, 
+      c(
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        # median_income_demo_cr_scale,
+        # median_income_demo_st_scale,
+        # median_income_demo_iq_scale
+      )
+    )
+  
+  input_fun_lm <- function(data, outcome, predictor, covariates){
+    lm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data)
+  }
+  
+  input_fun_glm <- function(data, outcome, predictor, covariates){
+    glm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data, family = binomial())
+  }
+  
+  models <- 
+    df %>% 
+    group_by(outcome_key, mi_var) %>% 
+    nest %>%
+    mutate(
+      model1 = 
+        map(
+          data, 
+          ~input_fun_lm(
+            data = .,
+            outcome = "scale(value)",
+            predictor = "scale(mi_value)",
+            covariates = 
+              c(
+                "scale(income)",
+                "year"
+              )
+          ) %>%
+            tidy %>%
+            dplyr::select(term, estimate) %>%
+            spread(term, estimate)
+        )
+    ) %>%  
+    dplyr::select(-data) %>% 
+    unnest(cols = c(model1))
+  
+  rm(df)
+  
+  return(models)
+}
+
+
+many_models_demographic_reference_glm_w_covariates <- function(outcomes, df){
+  df <-
+    df %>%
+    filter_at(
+      vars(
+        ladder_now_scale,
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        education_scale,
+        raw_income_scale,
+        median_home_value_county_scale,
+        median_monthly_housing_cost_county_scale,
+        physicians_scale,
+        total_pop_county_scale,
+        land_area_2010_scale,
+        race,
+        sex,
+        age_scale,
+        married,
+        employment_all,
+        year
+      ),
+      all_vars(!is.na(.))
+    )
+  
+  df <- 
+    df %>%
+    sample_n(500000) %>% 
+    gather(outcome_key, value, outcomes)
+  
+  df <-
+    df %>% 
+    gather(
+      mi_var, 
+      mi_value, 
+      c(
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        # median_income_demo_cr_scale,
+        # median_income_demo_st_scale,
+        # median_income_demo_iq_scale
+      )
+    )
+  
+  input_fun_lm <- function(data, outcome, predictor, covariates){
+    lm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data)
+  }
+  
+  input_fun_glm <- function(data, outcome, predictor, covariates){
+    glm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data, family = binomial())
+  }
+  
+  models <- 
+    df %>% 
+    group_by(outcome_key, mi_var) %>% 
+    nest %>%
+    mutate(
+      model1 = 
+        map(
+          data, 
+          ~input_fun_glm(
+            data = .,
+            outcome = "value",
+            predictor = "scale(mi_value)",
+            covariates = 
+              c(
+                "scale(income)", 
+                "scale(education)",
+                "scale(age)",
+                "employment_all",
+                "race",
+                "sex",
+                "married",
+                "scale(total_pop_county)",
+                "scale(median_monthly_housing_cost_county)",
+                "scale(land_area_2010)",
+                "physicians_scale",
+                "year"
+              )
+          ) %>%
+            tidy %>%
+            dplyr::select(term, estimate) %>%
+            spread(term, estimate)
+        )
+    ) %>%  
+    dplyr::select(-data) %>% 
+    unnest(cols = c(model1))
+  
+  rm(df)
+  
+  return(models)
+}
+
+
+many_models_demographic_reference_glm_wo_covariates <- function(outcomes, df){
+  df <-
+    df %>%
+    filter_at(
+      vars(
+        ladder_now_scale,
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        education_scale,
+        raw_income_scale,
+        median_home_value_county_scale,
+        median_monthly_housing_cost_county_scale,
+        physicians_scale,
+        total_pop_county_scale,
+        land_area_2010_scale,
+        race,
+        sex,
+        age_scale,
+        married,
+        employment_all,
+        year
+      ),
+      all_vars(!is.na(.))
+    )
+  
+  df <- 
+    df %>%
+    sample_n(500000) %>% 
+    gather(outcome_key, value, outcomes)
+  
+  df <-
+    df %>% 
+    gather(
+      mi_var, 
+      mi_value, 
+      c(
+        median_income_demo_sa_scale, 
+        median_income_demo_esa_scale, 
+        median_income_demo_esar_scale, 
+        median_income_demo_sar_scale, 
+        median_income_demo_saiq_scale, 
+        median_income_demo_sariq_scale, 
+        median_income_demo_esaiq_scale,
+        median_income_demo_sacr_scale, 
+        median_income_demo_sarcr_scale, 
+        median_income_demo_esacr_scale,
+        # median_income_demo_cr_scale,
+        # median_income_demo_st_scale,
+        # median_income_demo_iq_scale
+      )
+    )
+  
+  input_fun_lm <- function(data, outcome, predictor, covariates){
+    lm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data)
+  }
+  
+  input_fun_glm <- function(data, outcome, predictor, covariates){
+    glm(as.formula(paste(outcome, "~", predictor, "+", paste(covariates, collapse = "+"))), data = data, family = binomial())
+  }
+  
+  models <- 
+    df %>% 
+    group_by(outcome_key, mi_var) %>% 
+    nest %>%
+    mutate(
+      model1 = 
+        map(
+          data, 
+          ~input_fun_glm(
+            data = .,
+            outcome = "value",
+            predictor = "scale(mi_value)",
+            covariates = 
+              c(
+                "scale(income)",
+                "year"
+              )
+          ) %>%
+            tidy %>%
+            dplyr::select(term, estimate) %>%
+            spread(term, estimate)
+        )
+    ) %>%  
+    dplyr::select(-data) %>% 
+    unnest(cols = c(model1))
+  
+  rm(df)
+  
+  return(models)
+}
+
+linear_models_outcomes_wc <- many_models_demographic_reference_w_covariates(c("fruits_veggies_scale", "bmi_scale", "ladder_now_scale", "sr_health_scale"), dfg_rs)
+write_csv(linear_models_outcomes_wc, "D:/data/gallup/results/many_models_reference_income/linear_models_outcomes_wc.csv")
+
+linear_models_outcomes_woc <- many_models_demographic_reference_w_covariates(c("fruits_veggies_scale", "bmi_scale", "ladder_now_scale", "sr_health_scale"), dfg_rs)
+write_csv(linear_models_outcomes_woc, "D:/data/gallup/results/many_models_reference_income/linear_models_outcomes_woc.csv")
+
+linear_models_mediators_wc <- many_models_demographic_reference_mediators_w_covariates(c("PURPOSE_scale", "FINANCIAL_scale", "COMMUNITY_scale", "SOCIAL_scale"), dfg_rs)
+write_csv(linear_models_mediators_wc, "D:/data/gallup/results/many_models_reference_income/linear_models_mediators_wc.csv")
+
+linear_models_mediators_woc <- many_models_demographic_reference_mediators_wo_covariates(c("PURPOSE_scale", "FINANCIAL_scale", "COMMUNITY_scale", "SOCIAL_scale"), dfg_rs)
+write_csv(linear_models_mediators_woc, "D:/data/gallup/results/many_models_reference_income/linear_models_mediators_woc.csv")
+
+logistic_models_outcomes_wc <- many_models_demographic_reference_glm_w_covariates(c("eat_healthy", "smoke", "diabetes", "hbp"), dfg_rs)
+write_csv(logistic_models_outcomes_wc, "D:/data/gallup/results/many_models_reference_income/logistic_models_outcomes_wc.csv")
+
+logistic_models_outcomes_woc <- many_models_demographic_reference_glm_wo_covariates(c("eat_healthy", "smoke", "diabetes", "hbp"), dfg_rs)
+write_csv(logistic_models_outcomes_woc, "D:/data/gallup/results/many_models_reference_income/logistic_models_outcomes_woc.csv")
+
+
+
